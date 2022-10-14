457f172e8d4b4ea2641742d2baef5e46b93e7526
Sriraj Bura <sriraj.bura16@gmail.com>
2022-10-14 17:02:38 -0400

    use MutationObserver api to remove the title

--- after.js
+++ after.js
@@ -1,33 +1,18 @@
 // this code will be executed after page load
 (function () {
   console.log("after.js executed");
-  // .ytp-title
-  // Select the node that will be observed for mutations
-  const targetNode = document.getElementsByTagName("body");
 
-  // Options for the observer (which mutations to observe)
-  const config = { attributes: true, childList: true, subtree: true };
+  const targetNode = document.querySelector("body");
 
-  // Callback function to execute when mutations are observed
-  const callback = (mutationList, observer) => {
-    // console.log(mutationList);
-    // I would want to delete the target node
-    for (const mutation of mutationList) {
-      if (mutation.type === "childList") {
-        console.log("A child node has been added or removed.");
-      } else if (mutation.type === "attributes") {
-        console.log(`The ${mutation.attributeName} attribute was modified.`);
-      }
+  const config = { childList: true, subtree: true };
+
+  const observer = new MutationObserver(function () {
+    const videoTitle = document.querySelector(".ytp-title");
+    if (videoTitle) {
+      videoTitle.remove();
+      observer.disconnect();
     }
+  });
 
-    console.log("mutation is added");
-  };
-
-  // Create an observer instance linked to the callback function
-  const observer = new MutationObserver(callback);
-
-  // Start observing the target node for configured mutations
   observer.observe(targetNode, config);
-
-  observer.disconnect();
 })();
